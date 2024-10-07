b9d994c9f724d5f8f1f7bdb505ee8513a0df58ee
IrynaShostenko <163306036+IrynaShostenko@users.noreply.github.com>
2024-10-07 13:32:49 +0300

    завдання_4

---
+++ task_4.py
@@ -0,0 +1,47 @@
+"""створюємо функцію, яка допоможе визначати, кого з колег потрібно привітати."""
+
+from datetime import datetime, timedelta
+
+
+def get_upcoming_birthdays(users):
+
+    # визначаємо поточну дату
+    today = datetime.today().date()
+
+    # визначаємо дату наступного тижня
+    next_week = today + timedelta(days=7)
+
+    # визначаємо дату понеділка наступного тижня
+    first_day_of_next_week = next_week - timedelta(days=next_week.weekday())
+
+    # створюємо список привітань
+    upcoming_birthdays = []
+
+    # перебираємо користувачів
+    for user in users:
+        user_birthday = datetime.strptime(user["birthday"], "%Y.%m.%d").date()
+        congratulation_date = user_birthday.replace(year=today.year)
+
+        if congratulation_date >= today and congratulation_date <= next_week:
+            if congratulation_date.weekday() <= 4:
+                upcoming_birthdays.append({"name": user["name"], "congratulation_date": congratulation_date.strftime("%Y.%m.%d")})
+            elif congratulation_date.weekday() == 5 or 6:
+                upcoming_birthdays.append({"name": user["name"], "congratulation_date": first_day_of_next_week.strftime("%Y.%m.%d")})
+      
+    # повертаємо список привітань
+    return upcoming_birthdays
+
+# отримуємо вхідні дані
+users = [
+    {"name": "John Doe", "birthday": "1985.10.08"},
+    {"name": "Jane Smith", "birthday": "1990.10.11"},
+    {"name": "Bob Johnson", "birthday": "1995.05.20"},
+    {"name": "Alice Williams", "birthday": "1988.10.13"},
+    {"name": "Mike Brown", "birthday": "1992.03.15"},
+]
+
+# викликаємо функцію   
+upcoming_birthdays = get_upcoming_birthdays(users)
+
+# виводимо список привітань на цей та наступний тиждень
+print("Список привітань на цьому тижні:", sorted(upcoming_birthdays, key=lambda dates: dates["congratulation_date"]))
